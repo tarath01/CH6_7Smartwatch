6ccf801f2abffee53e48846e0381910864724bb5
tarath01 <epstrath01@gmail.com>
2026-03-03 15:16:58 -0600

    variables and loops

--- faqs_pseudocode.js
+++ faqs_pseudocode.js
@@ -1,41 +1,75 @@
+/*
+=================================================
+Smart Watch FAQ's
+Author: Taylor Rath
+Date: 03/03/2026
+GitHub: https://github.com/tarath01/CH6_7Smartwatch
+Program Summary: FAQ and Image Swap to create a Smartwatch Pro FAQ
+page that includes image swap.  It must display the correct image
+based on the FAQ selected or reset to the default image when all
+FAQ are closed.
+===================================================
+ */
+
+"use strict"
+
 // SETUP GLOBAL VARIABLES
-1. faqImage = Find the main FAQ image element and save it to a variable using querySelector
-2. faqImageOrigSrc = Save the main FAQ image's default 'src' to a variable using the src property
-3. faqImageOrigAtl = Save the main FAQ image's default 'alt' text to a variable using the alt property
-4. h2s = Find all the H2 elements inside the FAQ section and save them as a list using querySelectorAll "#faqs h2"
+//1. faqImage = Find the main FAQ image element and save it to a variable using querySelector
+//2. faqImageOrigSrc = Save the main FAQ image's default 'src' to a variable using the src property
+//3. faqImageOrigAtl = Save the main FAQ image's default 'alt' text to a variable using the alt property
+//4. h2s = Find all the H2 elements inside the FAQ section and save them as a list using querySelectorAll "#faqs h2"
+
+
+const faqImage = document.querySelector;
+const h2s = document.querySelectorAll("#faqs h2");
 
 // SETUP THE EVENT HANDLER (When an H2 is clicked)
 const toggleVisibility = evt => {
-    1. Figure out exactly which H2 heading the user just clicked and save it to a variable using 
-	   const ct = evt.currentTarget;
-	
-    2. Create a variable called 'allClosed' and set it to FALSE
-	   this will be used to reset the default image if all FAQs are closed
-    
-    3. LOOP through every H2 heading (h2s) in your saved list
-    
+
+  /* 1. Figure out exactly which H2 heading the user just clicked and save it to a variable using*/
+	   const h2 = evt.currentTarget;
+       const div = h2.nextElementSibling;
+
+ /*   2. Create a variable called 'allClosed' and set it to FALSE
+	   this will be used to reset the default image if all FAQs are closed*/
+        document.querySelector("#allClosed").disabled = false;
+
+   /* 3. LOOP through every H2 heading (h2s) in your saved list
+
         IF the current heading in the loop is the one the user clicked:
-            - Check if this heading already has the "minus" class. 
+            - Check if this heading already has the "minus" class.
               If it does, change your 'allClosed' tracker to TRUE.
-              
+
             - Toggle the "minus" class on this clicked heading.
-            - Toggle the "open" class on the answer element directly below it.
-            
-            - Get the new image source stored in this heading's 'data-img' attribute.
+            - Toggle the "open" class on the answer element directly below it.*/
+            h2.classList.toggle("minus");
+            div.classList.toggle("open");
+
+ /*           - Get the new image source stored in this heading's 'data-img' attribute.
             - Get the new alt text stored in this heading's 'data-alt' attribute.
             - Update the main FAQ image to show this new source and alt text.
-            
-        ELSE (it is a different heading):
-            - Remove the "minus" class from this heading.
-            - Remove the "open" class from the answer element directly below it.
-            
-    4. IF your 'allClosed' tracker is TRUE:
-        - Change the main FAQ image's source back to the original default source.
-        - Change the main FAQ image's alt text back to the original default alt text.
-        
-    5. Prevent the browser's default click behavior.
 
+    ELSE (it is a different heading):
+            - Remove the "minus" class from this heading.
+            - Remove the "open" class from the answer element directly below it.*/
+    else {
+
+    }
+
+ /*  4. IF your 'allClosed' tracker is TRUE:
+        - Change the main FAQ image's source back to the original default source.
+       - Change the main FAQ image's alt text back to the original default alt text.*/
+    if("#allClosed" === true) {
+
+    }
+  /*  5. Prevent the browser's default click behavior.*/
+    evt.preventDefault(); // prevents default click behavior
+}
 // INITIALIZATION - WHEN THE PAGE LOADS
 document.addEventListener("DOMContentLoaded", () => {
-    1. LOOP through your saved list of all h2 headings (hs2).
-    2. For each heading, attach a "click" event listener that triggers the 'toggleVisibility' function.
+    for (let h2 of h2s) {
+        h2.addEventListener("click", toggleVisibility);
+    }
+});
+     //   1. LOOP through your saved list of all h2 headings (hs2).
+      //  2. For each heading, attach a "click" event listener that triggers the 'toggleVisibility' function.
