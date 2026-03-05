4754ccedd738fd77a394d21b1be632e337ff5b8d
tarath01 <epstrath01@gmail.com>
2026-03-05 15:23:25 -0600

    image change

--- faqs_pseudocode.js
+++ faqs_pseudocode.js
@@ -19,30 +19,46 @@ FAQ are closed.
 //3. faqImageOrigAtl = Save the main FAQ image's default 'alt' text to a variable using the alt property
 //4. h2s = Find all the H2 elements inside the FAQ section and save them as a list using querySelectorAll "#faqs h2"
 
-const faqImage = document.querySelector("#faq_image");
+let faqImage = document.querySelector("#faq_image");
 const h2s = document.querySelectorAll("#faqs h2");
-const faqImageOrigSrc = faqImage.src;
-const faqImageOrigAlt = faqImage.alt;
+const faqImageOrigSrc = faqImage.getAttribute("src");
+const faqImageOrigAlt = faqImage.getAttribute("alt");
 
 
 // SETUP THE EVENT HANDLER (When an H2 is clicked)
 const toggleVisibility = evt => {
 
     /* 1. Figure out exactly which H2 heading the user just clicked and save it to a variable using*/
-    const h2 = evt.currentTarget;
-    const div = h2.nextElementSibling;
+    const selectedH2 = evt.currentTarget;
+    const div = selectedH2.nextElementSibling;
 
+    let allClosed = false;
     /*   2. Create a variable called 'allClosed' and set it to FALSE
           this will be used to reset the default image if all FAQs are closed*/
 
-    let allClosed = false;
     /* 3. LOOP through every H2 heading (h2s) in your saved list*/
     for (let h2 of h2s) {
-        h2.addEventListener("click", toggleVisibility);
-    }
-    h2.classList.toggle("minus");
-    div.classList.toggle("open");
+        if (h2 === selectedH2) {
+            if (h2.classList.contains("minus")) allClosed = true;
 
+            h2.classList.toggle("minus");
+            div.classList.toggle("open");
+
+            let dataImg = h2.getAttribute("data-img");
+            let dataAlt = h2.getAttribute("data-alt");
+
+            faqImage.setAttribute("src", dataImg);
+            faqImage.setAttribute("alt", dataAlt);
+        } else {
+            h2.classList.remove("minus");
+            h2.classList.remove("open");
+        }
+    }
+    if (allClosed === true) {
+        faqImage.setAttribute("src", faqImageOrigSrc);
+        faqImage.setAttribute("alt", faqImageOrigAlt);
+    }
+    evt.preventDefault();
 
 
     /*   IF the current heading in the loop is the one the user clicked:
@@ -68,13 +84,13 @@ const toggleVisibility = evt => {
 }
 
 // INITIALIZATION - WHEN THE PAGE LOADS
-    document.addEventListener("DOMContentLoaded", () => {
-        for (let h2 of h2s) {
-            h2.addEventListener("click", toggleVisibility);
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    for (let h2 of h2s) {
+        h2.addEventListener("click", toggleVisibility);
+    }
 
-        h2s[0].focus();
-    });
+    h2s[0].focus();
+});
 
 //   1. LOOP through your saved list of all h2 headings (hs2).
 //  2. For each heading, attach a "click" event listener that triggers the 'toggleVisibility' function.
